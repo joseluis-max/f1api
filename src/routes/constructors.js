83b1dd1b2f6732c9e11ed6921e3d374167857f19
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-04-17 14:32:51 -0500

    Add endpoints of constructors

--- src/routes/constructors.js
+++ src/routes/constructors.js
@@ -5,6 +5,29 @@ const constructors = require('../../db/constructors.json');
 
 router.get('/constructors', (req, res) => {
   res.json(constructors);
-})
-
+});
+router.post('/constructors',(req,res) => {
+  constructors['Constructors'].push(req.body);
+  res.json(constructors);
+});
+router.delete('/constructors/:id', (req, res) => {
+  const { id } = req.params;
+  constructors['Constructors'].forEach( c => {
+    if(c.constructorId == id){
+      constructors['Constructors'].splice(constructors['Constructors'].indexOf(c), 1);
+    }
+  });
+  res.json(constructors);
+});
+router.put('/constructors/:id', (req, res) => {
+  const { id } = req.params;
+  constructors['Constructors'].forEach(c => {
+    if(c.constructorId == id ){
+      for (const k in c) {
+        c[k]=req.body[k];
+      }
+    }
+  });
+  res.json(constructors);
+});
 module.exports = router;
